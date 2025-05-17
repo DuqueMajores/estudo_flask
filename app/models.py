ce6e4abb8b7e9c77afc7bf2267960b43008494cb
DuqueMajores <duque.majores@gmail.com>
2025-05-17 11:59:50 -0300

    building database

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+
+class Contato(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, nullable=True)
+    assunto = db.Column(db.String, nullable=True)
+    mensagem = db.Column(db.String, nullable=True)
+    
+
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, request
 
 @app.route("/")
 def homepage():
-    return render_template('index.html')
+    context = {
+        'usuario':'Moises',
+        'idade':28
+    }
+    return render_template('index.html', context=context)
 
+@app.route("/formulario")
+def formulario():
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        context.update({'pesquisa':pesquisa})
+    return render_template('contato.html', context=context)
